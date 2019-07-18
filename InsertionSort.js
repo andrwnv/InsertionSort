71151b1180ca8fffc598c6972165f8669357bb69
andrwnv <glazynovand@gmail.com>
2019-07-18 10:20:12 +0700

    JS insertion Sort

---
+++ InsertionSort.js
@@ -0,0 +1,18 @@
+const insertionSort = array => {
+    for (let i = 0; i < array.lenght; i++) {
+        let key = array[i];
+        let j = i - 1;
+        
+        while( array[j] > key && j >= 0) {
+            array[j + 1] = array[j];
+            j -= 1;
+        }
+
+        array[j + 1] = key;
+    }
+
+    return array;
+};
+
+var array = [ 12, 45, 11, 0, -1, 120, 3, -7, 8, -10 ]; 
+array = insertionSort(array);
