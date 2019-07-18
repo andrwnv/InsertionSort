6a38a41f2a40962607b4fc6d744a1fbc4f038511
andrwnv <glazynovand@gmail.com>
2019-07-18 10:01:24 +0700

    C# insertion Sort

---
+++ InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿namespace InsertionSort
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            int [] array = { 12, 45, 11, 0, -1, 120, 3, -7, 8, -10 };
+            insertionSort(array);
+        }
+
+        static void insertionSort(int [] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = i - 1;
+
+                while (j >= 0 && array[j] > key)
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+
+                array[j + 1] = key;
+            }
+        }
+    }
+}
