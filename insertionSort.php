32b652eb1c6664717c230cd12e52329b88fa5c7f
andrwnv <glazynovand@gmail.com>
2019-07-28 23:32:32 +0700

    PHP insertion Sort

---
+++ insertionSort.php
@@ -0,0 +1,20 @@
+<$php
+    $arr = array( 12, 45, 11, 0, -1, 120, 3, -7, 8, -10 );
+
+    function insertionSort($array) {
+        for ($index = 0; $index < count($array); $index++) {
+            $key = $array[$index];
+            $j = $index - 1;
+
+            while ($j >= 0 && $array[$j] > $key) {
+                $array[$j + 1] = $array[$j];
+                $j--;
+            }
+
+            $array[$j + 1] = $key;
+        }
+
+        return $array;
+    }
+
+    $array = insertionSort($array);
